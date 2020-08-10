ecc137af8ac317544a03c79685d71feeaea1e439
Natalia Takahara <nana_takahara@hotmail.com>
2020-08-10 17:09:41 +0900

    changed image size

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :preparations, reject_if: :all_blank, allow_destroy: true
   validates :title, :description, presence: true
-  has_attached_file :image, styles: { medium: "400x400#" }
+  has_attached_file :image, styles: { medium: "300x300#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   DIFFICULTIES = ["easy", "moderate", "challenging"]
   validates :difficulty, inclusion: {in: DIFFICULTIES}
