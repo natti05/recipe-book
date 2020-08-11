1b62987184f4d49e1678b327a24d6501e038483c
Natalia Takahara <nana_takahara@hotmail.com>
2020-08-11 18:43:02 +0900

    css for form input done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,6 @@ class Recipe < ApplicationRecord
   validates :title, :description, presence: true
   has_attached_file :image, styles: { medium: "300x300#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  DIFFICULTIES = ["easy", "moderate", "challenging"]
+  DIFFICULTIES = ["Easy", "Moderate", "Challenging"]
   validates :difficulty, inclusion: {in: DIFFICULTIES}
 end
